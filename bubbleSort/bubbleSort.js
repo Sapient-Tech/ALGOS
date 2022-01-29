5031b228fd591626fae2f73b2547f189fb4580c9
Amine GABSI <mamine.gabsi@gmail.com>
2022-01-29 09:55:44 +0100

    bubbleSort

--- bubbleSort/bubbleSort.js
+++ bubbleSort/bubbleSort.js
@@ -16,5 +16,20 @@ const nums2 = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
 const nums3 = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
 
 function bubbleSort(nums) {
-  // TODO
+  var check = true
+  var check1 = true 
+  while(check || check1){
+    for (let i = 0; i < nums.length; i++) {
+      if(nums[i] > nums[i+1]){
+        var x = nums[i+1]
+        nums[i+1] = nums[i]
+        nums[i] = x
+        check1 = check
+        check = true
+      } else{
+        check = false
+      }
+    }
+  }
+  return nums
 }
